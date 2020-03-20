3d277b887d8f33310f43b417dba533401552fc98
En Yi <randomsity1@gmail.com>
2020-03-20 20:26:54 +0800

    Put linked list for separate object

--- obj/player.c
+++ obj/player.c
@@ -322,3 +322,61 @@ void player_input_check(struct player_obj *player){
     else
         player->image->color.b = 0;
 }
+
+void create_afterimage(struct player_obj *player, Color color){
+    struct afterImage *img = malloc(sizeof(struct afterImage));
+    if (img){
+       for (int i=0;i<=BEZIER_POINTS;++i){
+            img->top_vertices[i] = player->image->top_vertices[i];
+            img->bottom_vertices[i] = player->image->bottom_vertices[i];
+            img->left_vertices[i] = player->image->left_vertices[i];
+            img->right_vertices[i] = player->image->right_vertices[i];
+        }
+        img->pos = (Vector2){player->image->center.x, player->image->center.y};
+        img->velocity = (Vector2){player->kinematic.velocity.x, player->kinematic.velocity.y};
+        img->opacity = 1.0;
+        img->color = color;
+        img->prev = NULL;
+        if (player->after_img_head == NULL){
+            img->next = NULL;
+            player->after_img_head = img;
+            player->after_img_tail = img;
+        }else{
+            img->next = player->after_img_head;
+            player->after_img_head->prev = img;
+            player->after_img_head = img;
+        } 
+    }
+}
+
+void remove_last_afterimage(struct player_obj *player){
+    struct afterImage *last;
+    if (player->after_img_tail != NULL){
+        if (player->after_img_tail->opacity <= 0){
+            last = player->after_img_tail;
+            player->after_img_tail = player->after_img_tail->prev;
+            last->prev = NULL;
+            free(last);
+        }
+        // This happens if the last tail is the head
+        if (player->after_img_tail == NULL){
+            player->after_img_head = NULL;
+        }else{
+            player->after_img_tail->next = NULL;
+        }
+    }
+}
+
+void free_afterimages(struct player_obj *player){
+    struct afterImage *current = player->after_img_head;
+    struct afterImage *next;
+    while(current != NULL){
+        next = current->next;
+        current->next = NULL;
+        current->prev = NULL;
+        free(current);
+        current = next;
+    }
+    player->after_img_head = NULL;
+    player->after_img_tail = NULL;
+}
